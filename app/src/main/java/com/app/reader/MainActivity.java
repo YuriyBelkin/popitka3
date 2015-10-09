8beb964f98fecf61cbd58f45ff3b9718a93a1a2b
sen123456 <867742202@qq.com>
2015-10-09 18:14:49 +0800

    BookReader项目

---
+++ app/src/main/java/com/app/reader/MainActivity.java
@@ -0,0 +1,159 @@
+package com.app.reader;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.app.reader.book.BooksFragment;
+import com.app.reader.widget.BackHandledFragment;
+
+
+public class MainActivity extends AppCompatActivity implements BackHandledFragment.BackHandlerInterface {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar mToolbar;
+    private BackHandledFragment selectedFragment;
+    private NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        //设置DrawerLayout
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        //设置navigationView的点击事件
+        mNavigationView = (NavigationView) findViewById(R.id.navigation_view);
+        setupDrawerContent(mNavigationView);
+
+        switchToBook();
+
+        //profile Image
+        setUpProfileImage();
+
+    }
+
+    private void switchToBook() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new BooksFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_book);
+    }
+
+    private void switchToExample() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new ExampleFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_example);
+    }
+
+
+
+    private void switchToAbout() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_content, new AboutFragment()).commit();
+        mToolbar.setTitle(R.string.navigation_about);
+    }
+
+
+    private void setUpProfileImage() {
+        findViewById(R.id.profile_image).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               switchToAbout();
+                mDrawerLayout.closeDrawers();
+                mNavigationView.getMenu().getItem(1).setChecked(true);
+            }
+        });
+    }
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+
+                            case R.id.navigation_item_book:
+                                switchToBook();
+                                break;
+                            case R.id.navigation_item_example:
+                                switchToExample();
+                                break;
+                            case R.id.navigation_item_about:
+                                switchToAbout();
+                                break;
+
+                        }
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setSelectedFragment(BackHandledFragment backHandledFragment) {
+        this.selectedFragment = backHandledFragment;
+    }
+
+
+    private long exitTime = 0;
+
+    public void doExitApp() {
+        if ((System.currentTimeMillis() - exitTime) > 2000) {
+            Toast.makeText(this, R.string.press_again_exit_app, Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            finish();
+        }
+    }
+
+    //点击返回键
+    @Override
+    public void onBackPressed() {
+        if (selectedFragment == null || !selectedFragment.onBackPressed()) {
+            //判断抽屉是否打开，如果打开，则关闭抽屉；否则退出应用
+            if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                mDrawerLayout.closeDrawer(Gravity.LEFT);
+            } else {
+                doExitApp();
+            }
+        }
+
+    }
+}
